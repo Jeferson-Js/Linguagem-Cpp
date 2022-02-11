41841d08ef0cdf19cc7fb3a8a8f7910997d27630
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-02-10 21:29:10 -0300

    Programando em C++

---
+++ 004.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //Declarando uma variavel do tipo inteiro.
+    int NumeroInteiro = 100;
+
+    cout << "NumeroInteiro";
+
+    return 0;
+}

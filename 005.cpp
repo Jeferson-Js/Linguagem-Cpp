bdd02904a202171084a7fe8adf7c489dbe13c4cc
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-02-10 21:35:00 -0300

    Programando em C++

---
+++ 005.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int x;
+
+    cout << "Digite o seu nome: ";
+    cin >> x;
+    cout << "Seja muito bem vindo a nossa comunidade" << x;
+
+    return 0;
+}
